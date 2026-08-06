1cfe29128dc82f18fdc8795ff04f80037e268a4b
Kamari93 <kmoore971@gmail.com>
2026-08-05 19:02:34 -0500

    Added get_all_activites and _row_to_activity helper fn in Activity repo

--- app/database.py
+++ app/database.py
@@ -3,6 +3,7 @@
 
 from app.models import Activity
 from uuid import UUID
+from datetime import datetime
 
 import sqlite3
 from pathlib import Path
@@ -74,7 +75,18 @@ class ActivityRepository:
 
     def get_all_activities(self) -> list[Activity]:
         '''Return all stored activities.'''
-        pass
+        cursor = self.connection.cursor()
+        cursor.execute(
+            '''
+            SELECT *
+            FROM activities
+            ORDER BY date DESC
+            '''
+        )
+
+        rows = cursor.fetchall()
+
+        return [self._row_to_activity(row) for row in rows]
 
     def get_activity_by_id(self, activity_id: UUID) -> Activity | None:
         '''Return a single activity by its ID.'''
@@ -87,3 +99,17 @@ class ActivityRepository:
     def delete_activity(self, activity_id: UUID) -> bool:
         '''Delete an activity.'''
         pass
+
+    def _row_to_activity(self, row: sqlite3.Row) -> Activity:
+        '''Convert a SQLite row into an Activity object.'''
+        activity = Activity(
+            activity_type=row["activity_type"],
+            distance=row["distance"],
+            duration=row["duration"],
+            notes=row["notes"],
+            route=row["route"],
+            )
+        activity.id = UUID(row["id"])
+        activity.date = datetime.fromisoformat(row["date"])
+
+        return activity
